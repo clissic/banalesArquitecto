fc52730c47b6d7da8accfacae170215d38e3bf07
Joaquin Perez <joaquin.perez.coria@gmail.com>
2024-01-21 01:38:36 -0300

    feat(async): Agregada asincronia a las funciones de fetch

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,51 +1,50 @@
 const projectsJSON = "../JSON/projects.json";
 const projectContainer = document.getElementById("projects-container");
 
-function fetchProjects() {
-    fetch("../JSON/projects.json")
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`Error: ${response.status} - ${response.statusText}`);
-        }
-        return response.json();
-    })
-    .then(data => {
-        const projects = data;
-        projects.forEach(project => {
-            if (project.outstanding === true) {
-                const projectHTML = `
-                <div class="col-md-6 col-lg-4 mb-4">
-                  <div class="card custom-card">
-                    <img
-                      src=${project.pictures[0]}
-                      class="card-img-top"
-                      alt="Imagen 1"
-                    />
-                    <a href="#">
-                      <div class="custom-card-overlay">
-                        <div class="card-horizontal-text">
-                          <h4 class="card-title text-uppercase">${project.name}</h4>
-                          <p class="card-text text-uppercase">
-                          ${project.type} <span class="text-secondary">/</span>
-                          </p>
-                          <p class="card-text card-text-location text-secondary text-uppercase">
-                          ${project.location}
-                          </p>
-                        </div>
-                        <div class="card-vertical-text">
-                          <p>VER MAS</p>
-                        </div>
+async function fetchProjects() {
+  try {
+      const response = await fetch(projectsJSON);
+
+      if (!response.ok) {
+          throw new Error(`Error: ${response.status} - ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      const projects = data;
+
+      projects.forEach(project => {
+          if (project.outstanding === true) {
+              const projectHTML = `
+                  <div class="col-md-6 col-lg-4 mb-4">
+                      <div class="card custom-card">
+                          <img src="${project.pictures[0]}" class="card-img-top" alt="Imagen 1" />
+                          <a href="#">
+                              <div class="custom-card-overlay">
+                                  <div class="card-horizontal-text">
+                                      <h4 class="card-title text-uppercase">${project.name}</h4>
+                                      <p class="card-text text-uppercase">
+                                          ${project.type} <span class="text-secondary">/</span>
+                                      </p>
+                                      <p class="card-text card-text-location text-secondary text-uppercase">
+                                          ${project.location}
+                                      </p>
+                                  </div>
+                                  <div class="card-vertical-text">
+                                      <p>VER MAS</p>
+                                  </div>
+                              </div>
+                          </a>
                       </div>
-                    </a>
                   </div>
-                </div>
-                `;
-                projectContainer.innerHTML += projectHTML;
-            }
-        });
-        console.log(projects);
-    })
-    .catch(error => console.error("Error fetching data:", error));
+              `;
+              projectContainer.innerHTML += projectHTML;
+          }
+      });
+
+      console.log(projects);
+  } catch (error) {
+      console.error("Error fetching data:", error);
+  }
 }
 
 fetchProjects()
--- assets/scripts/projects.js
+++ assets/scripts/projects.js
@@ -2,51 +2,49 @@ let projects = [];
 const projectsJSON = "../JSON/projects.json";
 const allProjectsContainer = document.getElementById("all-projects-container");
 
-function fetchAllProjects() {
-    fetch("../JSON/projects.json")
-    .then(response => {
+async function fetchAllProjects() {
+    try {
+        const response = await fetch("../JSON/projects.json");
+
         if (!response.ok) {
             throw new Error(`Error: ${response.status} - ${response.statusText}`);
         }
-        return response.json();
-    })
-    .then(data => {
+
+        const data = await response.json();
         projects = data;
+
         projects.forEach(project => {
-                const projectHTML = `
+            const projectHTML = `
                 <div class="col-md-6 col-lg-4 mb-4 project-card">
-                  <div class="card custom-card" data="${project.id}">
-                    <img
-                      src=${"." + project.pictures[0]}
-                      class="card-img-top"
-                      alt="Imagen 1"
-                    />
-                    <a href="#">
-                      <div class="custom-card-overlay">
-                        <div class="card-horizontal-text">
-                          <h4 class="card-title text-uppercase">${project.name}</h4>
-                          <p class="card-text text-uppercase">
-                          ${project.type} <span class="text-secondary">/</span>
-                          </p>
-                          <p class="card-text card-text-location text-secondary text-uppercase">
-                          ${project.location}
-                          </p>
-                        </div>
-                        <div class="card-vertical-text">
-                          <p>VER MAS</p>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
+                    <div class="card custom-card" data="${project.id}">
+                        <img src="${"." + project.pictures[0]}" class="card-img-top" alt="Imagen 1" />
+                        <a href="#">
+                            <div class="custom-card-overlay">
+                                <div class="card-horizontal-text">
+                                    <h4 class="card-title text-uppercase">${project.name}</h4>
+                                    <p class="card-text text-uppercase">
+                                        ${project.type} <span class="text-secondary">/</span>
+                                    </p>
+                                    <p class="card-text card-text-location text-secondary text-uppercase">
+                                        ${project.location}
+                                    </p>
+                                </div>
+                                <div class="card-vertical-text">
+                                    <p>VER MAS</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
                 </div>
-                `;
-                allProjectsContainer.innerHTML += projectHTML;
-            }
-        );
+            `;
+            allProjectsContainer.innerHTML += projectHTML;
+        });
+
         console.log(projects);
-        return projects
-    })
-    .catch(error => console.error("Error fetching data:", error));
+        return projects;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
 }
 fetchAllProjects()
 
