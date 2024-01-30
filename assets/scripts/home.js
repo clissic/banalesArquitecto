9a6e21609adc42b33d61e91f745c9b57cc748039
Joaquin Perez <joaquin.perez.coria@gmail.com>
2024-01-29 21:44:12 -0300

    fix(fetch): Intento 3

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,20 +1,20 @@
-const projectsJSON = "../../JSON/projects.json";
+/* const projectsJSON = "../../JSON/projects.json"; */
 const projectContainer = document.getElementById("projects-container");
 
-async function fetchProjects(URL) {
+/* async function fetchProjects(URL) {
   try {
-      const response = await fetch(URL);
+    const response = await fetch(URL);
 
-      if (!response.ok) {
-          throw new Error(`Error: ${response.status} - ${response.statusText}`);
-      }
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} - ${response.statusText}`);
+    }
 
-      const data = await response.json();
-      const projects = data;
+    const data = await response.json();
+    const projects = data;
 
-      projects.forEach(project => {
-          if (project.outstanding === true) {
-              const projectHTML = `
+    projects.forEach((project) => {
+      if (project.outstanding === true) {
+        const projectHTML = `
                   <div class="col-md-6 col-lg-4 mb-4">
                       <div class="card custom-card">
                           <img src="${project.pictures[0]}" class="card-img-top" alt="Imagen 1" />
@@ -37,14 +37,63 @@ async function fetchProjects(URL) {
                       </div>
                   </div>
               `;
-              projectContainer.innerHTML += projectHTML;
-          }
-      });
+        projectContainer.innerHTML += projectHTML;
+      }
+    });
 
-      console.log(projects);
+    console.log(projects);
   } catch (error) {
-      console.error("Error fetching data:", error);
+    console.error("Error fetching data:", error);
   }
 }
 
-fetchProjects(projectsJSON)
+fetchProjects(projectsJSON); */
+
+/* const jsonFilePath = "../JSON/projects.json"; */
+
+document.addEventListener("DOMContentLoaded", async () => {
+  // Realiza la solicitud fetch
+  await fetch("../assets/JSON/projects.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Error en la solicitud: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const projects = data;
+
+      projects.forEach((project) => {
+        if (project.outstanding === true) {
+          const projectHTML = `
+                  <div class="col-md-6 col-lg-4 mb-4">
+                      <div class="card custom-card">
+                          <img src="${project.pictures[0]}" class="card-img-top" alt="Imagen 1" />
+                          <a href="./pages/${project.url}">
+                              <div class="custom-card-overlay">
+                                  <div class="card-horizontal-text">
+                                      <h4 class="card-title text-uppercase">${project.name}</h4>
+                                      <p class="card-text text-uppercase">
+                                          ${project.type} <span class="text-secondary">/</span>
+                                      </p>
+                                      <p class="card-text card-text-location text-secondary text-uppercase">
+                                          ${project.location}
+                                      </p>
+                                  </div>
+                                  <div class="card-vertical-text">
+                                      <p>VER MAS</p>
+                                  </div>
+                              </div>
+                          </a>
+                      </div>
+                  </div>
+              `;
+          projectContainer.innerHTML += projectHTML;
+        }
+      });
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error en la solicitud fetch:", error);
+    });
+});
